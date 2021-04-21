6398ec5f11a669a0de257f3e1fbda38bf33a139b
BrunoCereda9979 <bruno9979@gmail.com>
2021-04-21 09:49:04 -0300

    Added middlewares to the server app.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 const express = require('express');
-const dotenv = require('dotenv').config()
+const dotenv = require('dotenv').config();
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
+
+// middlewares
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
 
 app.listen(process.env.APP_PORT || 3001, () => {
     console.log(`Server listening on port ${process.env.APP_PORT}...`);
