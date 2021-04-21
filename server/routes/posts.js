f3727bc585e4b4daa86c516a230015c51492f184
BrunoCereda9979 <bruno9979@gmail.com>
2021-04-21 16:34:30 -0300

    Created the route to get a post by id.

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../db/database');
 const Post = require('../models/Post');
 
+// get all posts
 router.get('/', (req, res) => {
     Post.findAll()
     .then(posts => { 
@@ -12,13 +13,36 @@ router.get('/', (req, res) => {
             })
         }
         else {
-            console.log(posts);
             res.status(200).json({
                 'posts': posts
             })
         }
     })
     .catch(err => console.log(err))
+});
+
+// get post by id
+router.get('/:id', (req, res) => {
+    const postId = req.params.id;
+    console.log(postId);
+    Post.findOne({ where: { post_id: postId } })
+    .then(post => {
+        if (post === null) {
+            res.status(204).json({
+                'message': 'Post not found'
+            })
+        }
+        else {
+            res.status(200).json({
+                'post': post
+            })
+        }
+    })
+    .catch(err => {
+        res.send(500).json({
+            'message': err.message
+        })
+    })
 });
 
 module.exports = router;
