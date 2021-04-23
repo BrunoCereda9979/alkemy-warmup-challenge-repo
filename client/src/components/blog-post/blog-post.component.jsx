f84870cf107df5758e78199334a528a94feb7425
BrunoCereda9979 <bruno9979@gmail.com>
2021-04-23 13:25:25 -0300

    Modified the post to only show the titles and chaged the title's font styles.

--- client/src/components/blog-post/blog-post.component.jsx
+++ client/src/components/blog-post/blog-post.component.jsx
@@ -7,7 +7,6 @@ const BlogPost = (props) => {
     return (
         <div className="post">
             <h1 className="post-title">{ props.title }</h1>
-            <p className="post-body">{ props.body }</p>
         </div>
     )
 }
