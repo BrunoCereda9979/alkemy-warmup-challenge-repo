93f69b2dbff9284e8d40a47e26875d1829deaa51
BrunoCereda9979 <bruno9979@gmail.com>
2021-04-22 21:29:30 -0300

    Added a feature to fetch all of the data when App component is loaded.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,33 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-
+import axios from 'axios';
 
 // components
 import PostsList from "./components/posts-list/posts-list.component";
 import BlogPost from "./components/blog-post/blog-post.component";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  // on load get the posts from the api
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then((posts) => {
+      setPosts(posts.data);
+    })
+    .catch(err => console.log(err.message))
+  }, []);
+
   return (
     <div className="App">
       <PostsList>
-        <BlogPost title="This is a blog title" body="This is a body for the blog post"/>
-        <BlogPost title="This is a blog title" body="This is a body for the blog post"/>
-        <BlogPost title="This is a blog title" body="This is a body for the blog post"/>
+        {
+          posts.map((post) => {
+            return(
+              <BlogPost key={post.id} title={post.title} body={post.body}/>
+            )
+          })
+        }
       </PostsList>
     </div>
   );
